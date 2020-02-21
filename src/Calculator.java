574c3fdd5314b934da98a82a9af64b3956e0b8e4
Dong Ha Kotwa <43553915+K-Hanna@users.noreply.github.com>
2020-02-21 13:36:17 +0100

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,80 +5,88 @@ class Calculator
 {
     static String calculate(String result){
 
-        if(result.matches(".*[(].*")){
-            String part1 = result.substring(0, result.indexOf('('));
-            String part2 = result.substring(result.indexOf('(') + 1, result.indexOf(')'));
-            String part3 = result.substring(result.indexOf(')') + 1);
+        try {
 
-            return calculate(part1 + calculate(part2) + part3);
-        }
+            if(result.matches("[0-9.]+"))
+                return result;
 
-        String counting;
+            if (result.matches(".*[(].*")) {
+                String part1 = result.substring(0, result.indexOf('('));
+                String part2 = result.substring(result.indexOf('(') + 1, result.indexOf(')'));
+                String part3 = result.substring(result.indexOf(')') + 1);
 
-        if(result.charAt(0)=='-')
-            counting = 0 + result;
-        else
-            counting = result;
-
-        String[] numbers = counting.split("\\s*[-/*+]+\\s*");
-        String[] signs = counting.split("\\s*[0-9.]+\\s*");
-
-        List<String> signList = new ArrayList<>();
-        List<Double> numbersList = new ArrayList<>();
-
-        for(int i = 0; i < numbers.length; i++){
-            numbersList.add(Double.valueOf(numbers[i]));
-            signList.add(signs[i]);
-        }
-
-        signList.add("&");
-
-        int a = 0;
-
-        for(String sign : signList){
-            if(sign.equals("/") || sign.equals("*")){
-                a++;
+                return calculate(part1 + calculate(part2) + part3);
             }
-        }
 
-        int b = 0;
+            String counting;
 
-        while(b < a){
-            for(int i = 0; i < signList.size(); i++){
-                if(signList.get(i).equals("/")){
-                    if(numbersList.get(i) == 0)
-                        return "Nie można dzielić przez 0!";
-                    numbersList.set(i - 1, numbersList.get(i - 1) / numbersList.get(i));
-                    numbersList.remove(i);
-                    signList.remove(i);
-                    b++;
-                }
-                if(signList.get(i).equals("*")){
-                    numbersList.set(i - 1, numbersList.get(i - 1) * numbersList.get(i));
-                    numbersList.remove(i);
-                    signList.remove(i);
-                    b++;
+            if (result.charAt(0) == '-')
+                counting = 0 + result;
+            else
+                counting = result;
+
+            String[] numbers = counting.split("\\s*[-/*+]+\\s*");
+            String[] signs = counting.split("\\s*[0-9.]+\\s*");
+
+            List<String> signList = new ArrayList<>();
+            List<Double> numbersList = new ArrayList<>();
+
+            for (int i = 0; i < numbers.length; i++) {
+                numbersList.add(Double.valueOf(numbers[i]));
+                signList.add(signs[i]);
+            }
+
+            signList.add("&");
+
+            int a = 0;
+
+            for (String sign : signList) {
+                if (sign.equals("/") || sign.equals("*")) {
+                    a++;
                 }
             }
-        }
 
-        while(signList.size() > 2){
-            for(int i = 0; i < signList.size(); i++){
-                if(signList.get(i).equals("+")){
-                    numbersList.set(i - 1, numbersList.get(i - 1) + numbersList.get(i));
-                    numbersList.remove(i);
-                    signList.remove(i);
-                    b++;
-                }
-                if(signList.get(i).equals("-")){
-                    numbersList.set(i - 1, numbersList.get(i - 1) - numbersList.get(i));
-                    numbersList.remove(i);
-                    signList.remove(i);
-                    b++;
+            int b = 0;
+
+            while (b < a) {
+                for (int i = 0; i < signList.size(); i++) {
+                    if (signList.get(i).equals("/")) {
+                        if (numbersList.get(i) == 0)
+                            return "Cannot divide by 0!";
+                        numbersList.set(i - 1, numbersList.get(i - 1) / numbersList.get(i));
+                        numbersList.remove(i);
+                        signList.remove(i);
+                        b++;
+                    }
+                    if (signList.get(i).equals("*")) {
+                        numbersList.set(i - 1, numbersList.get(i - 1) * numbersList.get(i));
+                        numbersList.remove(i);
+                        signList.remove(i);
+                        b++;
+                    }
                 }
             }
-        }
 
-        return Double.toString(numbersList.get(0));
+            while (signList.size() > 2) {
+                for (int i = 0; i < signList.size(); i++) {
+                    if (signList.get(i).equals("+")) {
+                        numbersList.set(i - 1, numbersList.get(i - 1) + numbersList.get(i));
+                        numbersList.remove(i);
+                        signList.remove(i);
+                        b++;
+                    }
+                    if (signList.get(i).equals("-")) {
+                        numbersList.set(i - 1, numbersList.get(i - 1) - numbersList.get(i));
+                        numbersList.remove(i);
+                        signList.remove(i);
+                        b++;
+                    }
+                }
+            }
+
+            return Double.toString(numbersList.get(0));
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException | StringIndexOutOfBoundsException e){
+            return "Error";
+        }
     }
 }
